43fab2314baeb74bf829f6d5a2bfac60d4ea24cf
Redddeve <popko.dmitry797@gmail.com>
2023-10-08 17:52:31 +0300

    Task complete

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,56 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected key: Key | undefined;
+  protected tenants: Person[] = [];
+
+  abstract openDoor(key: Key): void;
+
+  constructor(key: Key) {
+    this.door = false;
+    this.key = key;
+  }
+
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`${person} came in!`);
+    } else {
+      console.log('Door is closed!');
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (this.key?.getSignature() === key.getSignature()) {
+      this.door = true;
+      console.log('Key is correct, door is opened!');
+    } else {
+      console.log('Key is incorrect, door is closed!');
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +59,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
